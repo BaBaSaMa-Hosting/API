34680cff7d863966d74d905cedfbf6b300c9fe1d
BaBaSaMa <19013345@myrp.edu.sg>
2021-12-27 22:02:47 +0800

    create home management update

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ fastify.get('/', async (request, reply) => {
     reply.send("api.babasama.com")
 });
 
-fastify.register(require('./routes/bus/get_bus_route'));
-fastify.register(require('./routes/bus/get_nearest_bus_stop'));
-fastify.register(require('./routes/bus/get_bus_arrival'));
-fastify.register(require('./routes/bus/get_bus_stop_data'));
-fastify.register(require('./routes/bus/get_bus_data'));
+// fastify.register(require('./routes/bus/get_bus_route'));
+// fastify.register(require('./routes/bus/get_nearest_bus_stop'));
+// fastify.register(require('./routes/bus/get_bus_arrival'));
+// fastify.register(require('./routes/bus/get_bus_stop_data'));
+// fastify.register(require('./routes/bus/get_bus_data'));
 fastify.register(require('./routes/get_random_quote'));
+
+fastify.register(require('./routes/home-management/login'));
 
 const start = async() => {
     await fastify.register(require('middie'))
---
+++ routes/home-management/login.js
@@ -0,0 +1,30 @@
+const dotenv = require('dotenv');
+
+module.exports = async (fastify, opts) => {
+    const buf = Buffer.from('BASIC=basic')
+    const config = dotenv.parse(buf, {
+        path: './mysql.env'
+    })
+
+    fastify.register(require('fastify-mysql'), {
+        connectionString: `mysql://${config.username}@${config.host}/${config.database}`
+    })
+
+    fastify.get('/home-management/login', async function (request, reply) {
+        fastify.mysql.getConnection(onConnect)
+
+        let client;
+        const onConnect = (error, client) => {
+            if (error) return reply.send(error);
+            client = client
+        }
+
+        client.query('SELECT * FROM Users',
+            [],
+            onResult = (err, result) => {
+                client.release()
+                reply.send(err || result)
+            }
+        )
+    });
+}
