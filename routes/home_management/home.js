15befd11282ef7af2cc81b8ac542cf67a4a065e7
BaBaSaMa <19013345@myrp.edu.sg>
2022-01-19 20:41:28 +0800

    Update home.js

--- routes/home_management/home.js
+++ routes/home_management/home.js
@@ -320,11 +320,11 @@ module.exports = async (fastify, opts) => {
         const default_categories = ["242c1439-fe45-42ff-af17-92d3c240ec96", "2d776bca-6c07-4b61-9d4c-e916b76ce42a", "68792937-db58-485a-bfc6-1856b243407e", "9aa2e39b-eb4d-4689-9aa3-46d20c49250e", "c60bc19e-be0a-40ef-950c-fe8136018bf7"];
         
         new Promise((resolve, reject) => {
-            default_categories.forEach((category_id, index) => {
+            for (const [index, category_id] of default_categories.entries()) {
                 if (!await adding_category_into_home(reply, connection, request.body.home_id, request.body.user_id, category_id)) reject();
 
                 if (index === default_categories.length) resolve();
-            });
+            }
         }).catch(() => {
             return;
         });
